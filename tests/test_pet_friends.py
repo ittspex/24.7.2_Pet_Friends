97e9fd03a01f53e602938ebb798eba5b7ae63dfe
ittspex <153928261+ittspex@users.noreply.github.com>
2024-03-17 18:42:16 +0100

    Update test_pet_friends.py

--- tests/test_pet_friends.py
+++ tests/test_pet_friends.py
@@ -30,7 +30,7 @@ def test_get_all_pets_with_valid_key(filter=''):
 
 def test_add_new_pet_with_valid_data_cat(name='Васька', animal_type='кот',
                                      age='1', pet_photo='images/cat.pet.jpg'):
-    '''Проверяем гет запрос для добавления питомца с данными и файлом ,в данном случае кошки'''                                     
+    '''Проверяем гет запрос для добавления питомца с данными и файлом ,в данном случае кошки'''
 
     pet_photo = os.path.join(os.path.dirname(__file__), pet_photo)
     _, auth_key = pf.get_api_key(valid_email, valid_password)
@@ -57,6 +57,7 @@ def test_successful_update_pet_info_cat(name='Матрос', animal_type='cat', 
 
 def test_successful_delete_pet_cat():
     '''Проверяем дэлит запрос на удаление питомца, в данном случае кошки'''
+
     _, auth_key = pf.get_api_key(valid_email, valid_password)
     _, my_pets = pf.get_list_of_pets(auth_key, "my_pets")
 
@@ -84,7 +85,7 @@ def test_add_new_pet_with_valid_data_dog(name='Пёсбарбос', animal_type=
     assert result['name'] == name
 
 def test_successful_update_pet_info_dog(name='Волкодав', animal_type='Пёс', age=1):
-     '''Проверяем пут запрос на изменение данных питомца , в данном случае собаки'''
+    '''Проверяем пут запрос на изменение данных питомца , в данном случае собаки'''
 
     _, auth_key = pf.get_api_key(valid_email, valid_password)
     _, my_pets = pf.get_list_of_pets(auth_key, 'my_pets')
@@ -97,7 +98,7 @@ def test_successful_update_pet_info_dog(name='Волкодав', animal_type='П
         assert result['name'] == name
 
 def test_successful_delete_pet_dog():
-     '''Проверяем дэлит запрос на удаление питомца , в данном случае собаки'''
+    '''Проверяем дэлит запрос на удаление питомца , в данном случае собаки'''
     _, auth_key = pf.get_api_key(valid_email, valid_password)
     _, my_pets = pf.get_list_of_pets(auth_key, 'my_pets')
 
@@ -117,21 +118,21 @@ def test_successful_delete_pet_dog():
 def test_add_new_pet_with_invalid_photo(name='', animal_type='', age='', pet_photo='fotki/nofoto.jpg'):
     '''Проверяем, что неверный путь к файлу или несуществующий файл вызывают исключение'''
 
+
     _, auth_key = pf.get_api_key(valid_email, valid_password)
 
     try:
+
         status, result = pf.add_new_pet(auth_key, name, animal_type, age, pet_photo)
-         '''Добавляем питомца с несуществующим файлом'''
 
     except FileNotFoundError:
-       assert True
-         '''Проверяем, что исключение FileNotFoundError было вызвано'''
+        assert True
     else:
         assert False
-        '''Если исключение не было вызвано, то тест не прошел'''
 
 def test_post_create_pet_sipmle(name='Кузя', animal_type='Кот',age='9'):
-     '''Проверяем добавление питомца через пост запрос '''
+    '''Проверяем добавление питомца через пост запрос '''
+
     _, auth_key = pf.get_api_key(valid_email, valid_password)
     status, result = pf.create_pet_simple(auth_key, name, animal_type, age )
 
@@ -139,20 +140,21 @@ def test_post_create_pet_sipmle(name='Кузя', animal_type='Кот',age='9'):
     assert result['name'] == name
 
 def test_post_set_pet_photo(pet_photo='images\kuzja.pet.jpg'):
-     '''Проверяем добавление фото для питомца через пост запрос '''
+    '''Проверяем добавление фото для питомца через пост запрос '''
+
     _, auth_key = pf.get_api_key(valid_email, valid_password)
     _, my_pets = pf.get_list_of_pets(auth_key, "my_pets")
     pet_photo = os.path.join(os.path.dirname(__file__), pet_photo)
     pet_id = my_pets['pets'][0]['id']
 
+    # Проверяем, есть ли уже фотография у питомца
     if my_pets['pets'][0]['pet_photo'] == "":
-        ''' Проверяем, есть ли уже фотография у питомца '''
         status, result = pf.set_pet_photo(auth_key, pet_id, pet_photo)
         _, my_pets = pf.get_list_of_pets(auth_key, "my_pets")
         assert status == 200
         assert result['pet_photo'] == my_pets['pets'][0]['pet_photo']
     else:
-        print('Питомец имеет фото')
+        print("Питомец имеет фото")
 
 def test_unvalid_get_api_key_user(email=unvalid_email, password=unvalid_password):
     ''' Проверяем невозможность авторизации с ложными данными'''
